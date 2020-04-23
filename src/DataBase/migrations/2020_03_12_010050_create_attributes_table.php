b56671e7adec03bb8fb4c22d25c9fe5117945018
JerryLi <dream_imperator@163.com>
2020-04-23 17:17:08 +0800

    修改属性表备注错误

--- src/DataBase/migrations/2020_03_12_010050_create_attributes_table.php
+++ src/DataBase/migrations/2020_03_12_010050_create_attributes_table.php
@@ -19,7 +19,7 @@ class CreateAttributesTable extends Migration
             $table->string('attr_name', 20)->default('')->comment('属性名称');
             $table->string('attr_code', 20)->default('')->comment('属性标识');
             $table->string('input_type', 20)->default('input')->comment('内容录入方式');
-            $table->string('validate', 100)->default('input')->comment('内容录入方式');
+            $table->string('validate', 100)->default('input')->comment('内容验证方式');
             $table->enum('data_source', ['input', 'model', 'assign'])->default('input')->comment('数据来源');
             $table->text('place_holder')->comment('属性默认值及备选值');
             $table->timestamps();
